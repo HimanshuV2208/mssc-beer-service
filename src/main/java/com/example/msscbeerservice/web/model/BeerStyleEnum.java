108c3c5c669b50726dfb4bc8239e10dedd378e53
hvishwakarma <him2282001@gmail.com>
2023-05-29 16:37:45 +0530

    Added web data model.

---
+++ src/main/java/com/example/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package com.example.msscbeerservice.web.model;
+
+@SuppressWarnings("ALL")
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
